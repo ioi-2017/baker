c9dba83868f6601b4659f4763a4c6688eb1b7334
Hamed Valizadeh <havaliza@gmail.com>
2017-07-01 19:48:48 +0430

    open the last argument automatically if it ends in '.md' and is an absolute path

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ function createWindow() {
   // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
+  mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -39,8 +39,11 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', function() {
-  const file = server.openMarkdownFile();
+app.on('ready', function () {
+  const lastArgument = process.argv[process.argv.length - 1]
+  const { isAbsolute } = require('path')
+  const fileArgument = lastArgument.toLowerCase().endsWith('.md') && isAbsolute(lastArgument) ? lastArgument : null
+  const file = server.openMarkdownFile(fileArgument);
   if (file) {
     createWindow();
   } else {
@@ -49,7 +52,7 @@ app.on('ready', function() {
 });
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
+app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
@@ -57,7 +60,7 @@ app.on('window-all-closed', function() {
   }
 });
 
-app.on('activate', function() {
+app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
